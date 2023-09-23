af27476720123c8ae630b42a3af3fb5a7c5d8332
Chris <c.havrez@stag.technofuturtic.education>
2023-09-23 22:24:42 +0200

    Server_UpDated

--- server.js
+++ server.js
@@ -42,6 +42,26 @@ dbUtils.testDbConnection();
 // Création du serveur
 const server = http.createServer((request, response) => {
 
+    // Info de la requete
+    console.log(`url: "${request.url}" • method: "${request.method}"`);
+
+    // Routing simple (Méthode plus complexe vu avec "Express")
+    if(request.url === '/') {
+        // Appel de la méthode "index" en lui transmettant la requete et la réponse
+        homeController.index(request, response);
+    }
+    else if(request.url === '/message-add' && request.method === "GET") {
+        homeController.messageGET(request, response);
+    }
+    else if(request.url === '/message-add' && request.method === "POST") {
+        homeController.messagePOST(request, response);
+    }
+    else {
+        // Génération simple d'une page d'erreur 404 !
+        response.writeHead(404, { "Content-Type": "text/html" });
+        response.end("<h1>Page not found</h1>")
+    }
+});
 
 // Démarrer le serveur
 // Fonction qu'il va executer quand il va recevoir une information(listen =>lisener)
